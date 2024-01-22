14c7a2d3b8a1b8ffee3cd598be086824b240f429
HIMNISHKIRAN-CTI12 <87081224+HIMNISHKIRAN-CTI12@users.noreply.github.com>
2024-01-22 11:48:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function setup(){
+canvas = createCanvas(700,450);
+canvas.position(650,110);
+video = createCapture(VIDEO);
+video.size(500,500);
+}
+function draw() {
+    canvas.background(193,208,181);
+}
